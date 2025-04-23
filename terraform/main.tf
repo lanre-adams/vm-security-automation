d7584f805ba105d02c95b9ed152b3927e6ddc4ce
Olanrewaju Adams Agunloye <lanreadams88@gmail.com>
2025-04-23 14:27:41 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,39 @@
-# Placeholder for terraform/main.tf
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "vm_sec_rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_policy_definition" "enforce_tags" {
+  name         = "enforce-tags-policy"
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name = "Enforce resource tagging"
+  policy_rule  = file("${path.module}/policies/enforce_tags.json")
+  parameters   = jsonencode({ requiredTags = ["Environment", "Owner", "CostCenter"] })
+}
+
+resource "azurerm_policy_definition" "restrict_locations" {
+  name         = "restrict-locations-policy"
+  policy_type  = "Custom"
+  mode         = "All"
+  display_name = "Restrict resource locations"
+  policy_rule  = file("${path.module}/policies/restrict_locations.json")
+  parameters   = jsonencode({ allowedLocations = ["East US", "West Europe", "Central India"] })
+}
+
+resource "azurerm_policy_assignment" "tags_assignment" {
+  name                 = "enforce-tags-assignment"
+  policy_definition_id = azurerm_policy_definition.enforce_tags.id
+  scope                = azurerm_resource_group.vm_sec_rg.id
+}
+
+resource "azurerm_policy_assignment" "location_assignment" {
+  name                 = "restrict-locations-assignment"
+  policy_definition_id = azurerm_policy_definition.restrict_locations.id
+  scope                = azurerm_resource_group.vm_sec_rg.id
+}
