79dab2257b6292eae3b09fc8169231772439671b
Olanrewaju Adams Agunloye <lanreadams88@gmail.com>
2025-04-23 14:28:19 +0100

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,11 @@
-# Placeholder for terraform/variables.tf
+variable "resource_group_name" {
+  type        = string
+  default     = "vm-sec-automation-rg"
+  description = "Name of the Azure resource group"
+}
+
+variable "location" {
+  type        = string
+  default     = "East US"
+  description = "Azure location for deployment"
+}
